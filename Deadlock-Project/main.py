629929b65302058a76e31cd68a66a6d62a8c3f0d
Abdelrhman Alarabawy <83235049+abdelrhman-alarabawy@users.noreply.github.com>
2021-05-27 22:01:45 +0200

    Update main.py

--- Deadlock-Project/main.py
+++ Deadlock-Project/main.py
@@ -8,7 +8,7 @@ def detect(process, allocation, request, work):
         temp_list = []
         global check
         check = np.repeat([False], length)
-        
+       
         for i in range(len(process)):       
                 if(work[0] >= request[i][0] and work[1] >= request[i][1] and work[2] >= request[i][2] ):
                     work[0:3] = (work[0] + allocation[i][0]) , (work[1] + allocation[i][1]) , (work[2] + allocation[i][2])
@@ -17,25 +17,25 @@ def detect(process, allocation, request, work):
                     temp_list.append(i)
 #print(temp_list)
                         
-        for i in range(len(temp_list)):
+        for i in temp_list:
 #print("line 22 : i +  " , i)
             if(work[0] >= request[i][0] and work[1] >= request[i][1] and work[2] >= request[i][2] ):
                 work[0:3] = (work[0] + allocation[i][0]) , (work[1] + allocation[i][1]) , (work[2] + allocation[i][2])
                 check[i] = True
-#print(check)       
-        if any(check) == False:
-          return True   #deadlock found
+        print(check)       
+        if all(check) == True:
+          return True   #no deadlock found
         else:
-          return False  #no deadlock found
+          return False  #deadlock found
             
 if __name__=='__main__':
     
-    process=[1, 0, 45, 3, 4]
+    process=[0, 17, 2, 30, 4]
     allocation=[[0, 1, 0], [2, 0, 0], [3, 0, 3], [2, 1, 1], [0, 0, 2]]
-    request=[[0, 0, 0], [2, 0, 2], [0, 0, 1], [1, 0, 0], [0, 0, 2]]
+    request=[[0, 0, 0], [2, 0, 2], [0, 0, 0], [1, 0, 0], [0, 0, 2]]
     available=[0, 0, 0]
     result = detect(process, allocation, request, available)
-    
+    print(result)
 print("#"*50 + "\n\n")         
 
 if not (result):
@@ -49,3 +49,4 @@ else:
     print("System is not in the deadlock state")     
 
 print("\n\n" + "#"*50)    
+  
