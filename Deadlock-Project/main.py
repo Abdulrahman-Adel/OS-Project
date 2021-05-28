5dfbd09781cae97c6d0bd0fcd790af6563559aa9
Abdulrahman-Adel <57441828+Abdulrahman-Adel@users.noreply.github.com>
2021-05-28 11:43:29 +0200

    Update main.py

--- Deadlock-Project/main.py
+++ Deadlock-Project/main.py
@@ -1,52 +1,42 @@
-#from operator import add
-import numpy as np
+from operator import add
 
 def detect(process, allocation, request, work):
 	    
-   
         length = len(process)
         temp_list = []
-        global check
-        check = np.repeat([False], length)
+        check = [False] * length
        
-        for i in range(len(process)):       
-                if(work[0] >= request[i][0] and work[1] >= request[i][1] and work[2] >= request[i][2] ):
-                    work[0:3] = (work[0] + allocation[i][0]) , (work[1] + allocation[i][1]) , (work[2] + allocation[i][2])
+        for i in range(length):       
+                if all([work[idx] >= request[i][idx] for idx in range(len(work))]):
+                    work = work[0] + allocation[i][0], work[1] + allocation[i][1], work[2] + allocation[i][2]
                     check[i] = True
                 else:
                     temp_list.append(i)
-#print(temp_list)
                         
         for i in temp_list:
-#print("line 22 : i +  " , i)
-            if(work[0] >= request[i][0] and work[1] >= request[i][1] and work[2] >= request[i][2] ):
-                work[0:3] = (work[0] + allocation[i][0]) , (work[1] + allocation[i][1]) , (work[2] + allocation[i][2])
+            if all([work[idx] >= request[i][idx] for idx in range(len(work))]):
+                work = work[0] + allocation[i][0], work[1] + allocation[i][1], work[2] + allocation[i][2]
                 check[i] = True
-        print(check)       
-        if all(check) == True:
-          return True   #no deadlock found
+		temp_list.remove(i)
+	
+	print("#"*50 + "\n\n")   
+        if not temp_list:
+	  print("System is not in the deadlock state") 
+	  print("\n\n" + "#"*50) 
         else:
-          return False  #deadlock found
+	  print("the system is in the deadlock state ")
+          print("The Processes in the deadlock are : ")
+	  
+	  for i in temp_list:
+            print(f"Process {process[i]}")
+	print("\n\n" + "#"*50)   
             
 if __name__=='__main__':
     
-    process=[0, 17, 2, 30, 4]
+    process=[0, 1, 2, 3, 4]
     allocation=[[0, 1, 0], [2, 0, 0], [3, 0, 3], [2, 1, 1], [0, 0, 2]]
     request=[[0, 0, 0], [2, 0, 2], [0, 0, 0], [1, 0, 0], [0, 0, 2]]
     available=[0, 0, 0]
-    result = detect(process, allocation, request, available)
-    print(result)
-print("#"*50 + "\n\n")         
-
-if not (result):
-    print("the system is in the deadlock state ")
-    print("The Processes in the deadlock are : ")
     
-    for i in range(len(process)):
-        if not(check[i]): 
-            print(f"Process {process[i]}")
-else:
-    print("System is not in the deadlock state")     
-
-print("\n\n" + "#"*50)    
+    detect(process, allocation, request, available)
   
